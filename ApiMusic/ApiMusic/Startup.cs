ea3769880ff5c2d9af84c00f67a576775d51f898
Ronald Castillo <ronaldcastillo789@gmail.com>
2019-08-01 21:01:38 -0400

    swagger se ha integrado

--- ApiMusic/ApiMusic/Startup.cs
+++ ApiMusic/ApiMusic/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using Persistent;
 
 namespace ApiMusic
@@ -27,13 +29,33 @@ namespace ApiMusic
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
-           
 
             var connectionString = Configuration.GetConnectionString("dev");
-            //services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //configuracion de swagger
+            services.AddOpenApiDocument(document =>
+            {
+
+                document.Version = "v1";
+                document.Title = "Api music";
+                document.Description = "Api sobre la musica, donde los usuarios podran crear sus playlist";
+
+                //configuracion de JWT para swagger
+                document.AddSecurity("JWT", Enumerable.Empty<string>(),
+                    new NSwag.OpenApiSecurityScheme
+                    {
+                        Type = OpenApiSecuritySchemeType.ApiKey,
+                        Name = "Authorization",
+                        In = OpenApiSecurityApiKeyLocation.Header,
+                        Description = "Copia y pega el Token en el campo 'Value:' de esta manera, Bearer {Token JWT}."
+                    }
+
+                );
+                document.OperationProcessors.Add(
+                    new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +71,10 @@ namespace ApiMusic
             }
 
             app.UseHttpsRedirection();
+            //anadiendo swagger
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
             app.UseMvc();
         }
     }
