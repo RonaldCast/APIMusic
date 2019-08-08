e3dc3471547215d6241085ac12b40bc310343871
Ronald Castillo <ronaldcastillo789@gmail.com>
2019-08-08 14:37:02 -0400

    UnitTest configuration

---
+++ ApiMusic/XUnitTest/PersonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTest
+{
+
+    public class PersonController
+    {
+         
+        public PersonController()
+        {
+
+        }
+    }
+}
