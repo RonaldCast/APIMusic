b8328af8ef86a0e5215fedddd8b266fddf235c80
Ronald Castillo <ronaldcastillo789@gmail.com>
2019-08-09 07:37:56 -0400

    Se crearon los unit test de albumService

---
+++ ApiMusic/UnitTestProject1/ServicesStub/AlbumStub.cs
@@ -0,0 +1,44 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest.ServicesStub
+{
+    public static class AlbumStub
+    {
+        public static Album Album = new Album()
+        {
+            Title = "Los Ra",
+            Description = "To fuelte",
+            DatePublic = DateTimeOffset.Now,
+        };
+
+        public static Album UpdateAlbum = new Album()
+        {
+
+            Title = "EL final",
+            Description = "De los finales con el final",
+            DatePublic = DateTimeOffset.Now,
+        };
+
+        public static List<Album> Albums = new List<Album>()
+        {
+            new Album(){
+
+                Title = "EL final",
+                Description = "De los finales con el final",
+                DatePublic = DateTimeOffset.Now,
+            },
+
+             new Album(){
+
+                Title = "La independendicia",
+                Description = "Este trata acerca de una coleccion historica",
+                DatePublic = DateTimeOffset.Now,
+            }
+        }; 
+
+
+    }
+}
