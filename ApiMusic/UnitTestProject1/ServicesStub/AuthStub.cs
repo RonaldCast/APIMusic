53dd3f5e6f344551f592c115cbe414247fe698ca
Ronald Castillo <ronaldcastillo789@gmail.com>
2019-08-09 10:01:34 -0400

    Se realizo el unit testing authService

---
+++ ApiMusic/UnitTestProject1/ServicesStub/AuthStub.cs
@@ -0,0 +1,16 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+   public static class AuthStub
+    {
+        public static UserDTO user = new UserDTO()
+        {
+            Email = "ronald123@gmail.com",
+            Password = "123456"
+        };
+    }
+}
